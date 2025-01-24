920eda49d386429cc3649b4d1cad540a84da8be1
samuel.foster@diamond.ac.uk <samuel.foster@diamond.ac.uk>
2025-01-24 17:11:02 +0000

    Edited theme/parameters.yaml and Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -9,3 +9,14 @@ class Form1(Form1Template):
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
+
+  ##below not needed in final implementation but uswd for debugging
+  def radERAYes_clicked(self, **event_args):
+    """This method is called when this radio button is selected"""
+    print(self.radERAYes.get_group_value())
+    pass
+
+  def radERANo_clicked(self, **event_args):
+    """This method is called when this radio button is selected"""
+    print(self.radERANo.get_group_value())    
+    pass
