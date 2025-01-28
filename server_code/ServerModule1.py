49183137b1dd24907d405db6d91c0bde4713af64
samuel.foster@diamond.ac.uk <samuel.foster@diamond.ac.uk>
2025-01-28 15:08:27 +0000

    Edited ServerModule1

--- server_code/ServerModule1.py
+++ server_code/ServerModule1.py
@@ -1,4 +1,3 @@
-import anvil.email
 import anvil.server
 import anvil.pdf
 import gemmi
@@ -17,32 +16,6 @@ def check_email(email):
   else:
     return False
     
-# This is a server module. It runs on the Anvil server,
-# rather than in the user's browser.
-#
-# To allow anvil.server.call() to call functions here, we mark
-# them with @anvil.server.callable.
-# Here is an example - you can replace it with your own:
-#
-# @anvil.server.callable
-# def say_hello(name):
-#   print("Hello, " + name + "!")
-#   return 42
-#
-def send_feedback(name, email, feedback):
-  # Send yourself an email each time feedback is submitted
-  anvil.email.send(#to="vmxm@diamond.ac.uk", # Change this to your email address and remove the #!
-                   subject=f"Feedback from {name}",
-                   text=f"""
-                   
-  A new person has filled out the feedback form!
-
-  Name: {name}
-  Email address: {email}
-  Feedback:
-  {feedback}
-  """)
-
 @anvil.server.callable
 def create_pdf(name, email, affil, bag, contact, prot, seq, era, crystCond, 
                lcp, crystMethod, cryo, morph, size, density, number, sizing, sg, cell,
